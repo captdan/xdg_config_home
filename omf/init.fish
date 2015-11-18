a2b1c44270269dfe2c44f209dee3d71c5f4d82aa
captdan <gunhaver20x6@gmail.com>
2015-11-18 09:36:36 -0800

    host specific config

--- omf/init.fish
+++ omf/init.fish
@@ -1,7 +1,3 @@
-#Library Home Dirs
-set -x ANDROID_HOME /usr/local/opt/android-sdk
-set -x JAVA_HOME /Library/Java/Home
-
 #PATHs
 set -x PATH $HOME/.local/bin $PATH
 
---
+++ omf/platforms/Darwin.fish
@@ -0,0 +1,3 @@
+#Library Home Dirs
+set -x ANDROID_HOME /usr/local/opt/android-sdk
+set -gx JAVA_HOME /Library/Java/Home
